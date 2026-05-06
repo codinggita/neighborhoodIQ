0e8b3c5173e7712e506c252fa1654c70efe877db
Devisingh Rajput <devisinghrajput.cg@gmail.com>
2026-05-06 22:45:21 +0530

    feat: add user registration and login logic

---
+++ server/src/services/auth.service.js
@@ -0,0 +1,34 @@
+const httpStatus = require('http-status');
+const User = require('../models/User.model');
+const ApiError = require('../utils/ApiError');
+
+/**
+ * Login with username and password
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<User>}
+ */
+const loginUserWithEmailAndPassword = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user || !(await user.isPasswordMatch(password))) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
+  }
+  return user;
+};
+
+/**
+ * Create a user
+ * @param {Object} userBody
+ * @returns {Promise<User>}
+ */
+const createUser = async (userBody) => {
+  if (await User.isEmailTaken(userBody.email)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+  }
+  return User.create(userBody);
+};
+
+module.exports = {
+  loginUserWithEmailAndPassword,
+  createUser,
+};
